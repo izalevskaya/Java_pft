4c37b7d39128559a5a9ec677d218f36279113df3
izalevskaya <60923019+izalevskaya@users.noreply.github.com>
2020-05-22 18:09:36 +0600

    Revert "Создание классов"
    This reverts commit 2c743adc4850e3b1194177ca34299d7ad4e3b6ef.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
@@ -2,22 +2,32 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProg {
 
+
   public static void main(String[] args) {
     hello("world");
     hello("user");
     hello("Alexei");
     hello("Irina");
 
-    Square s = new Square(5);
-    System.out.println ("Площадь квадрата со стороной " + s.l + " = " + s.area() );
+    double l = 5;
+      System.out.println ("Площадь квадрата со стороной " + l + " = " + area(l) );
 
-    Rectangle r = new Rectangle(4,6);
-    System.out.println ("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = "+r.area());
+      double a = 4;
+      double b = 6;
+    System.out.println ("Площадь прямоугольника со сторонами " + a + " и " + b + " = "+ area(a, b));
 
-  }
+      }
 
   public static void hello (String somebody) {
     System.out.println("Hello,"+somebody + "!");
 
+  }
+
+  public static double area(double len) {
+    return len*len;
+  }
+
+  public  static  double area (double a, double b){
+    return a*b;
   }
 }
