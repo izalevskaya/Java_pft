5b953d24e5ccd1986ae0fa54fd515d2986cbfec2
izalevskaya <60923019+izalevskaya@users.noreply.github.com>
2020-08-13 15:26:19 +0600

    GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adddressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adddressbook/GroupCreationTests.java
@@ -19,7 +19,7 @@ public class GroupCreationTests {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
+    driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:\\Users\\user\\Desktop\\firefox-52.0esr.win64.sdk\\firefox-sdk\\bin\\firefox.exe"));
     baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
